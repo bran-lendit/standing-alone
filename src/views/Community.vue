698c95893b9894ae7cc710e7f5eb0635e14b6524
dudwls <ccd1202@naver.com>
2019-05-18 01:27:19 +0900

    updated2

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="about">
     <button>Community</button>
+  
     <hr>
     <article class="questions-item">
-      <h1 class="questions-item__title text-heading-5 bold text-black">거실 전면인테리어 ㅜㅜ도와주세요</h1>
+      <h1 class="questions-item__title text-heading-5 bold text-black"><router-link to="communitydetail">거실 전면인테리어 ㅜㅜ도와주세요</router-link></h1>
             <div class="questions-item__image">
               <div class="image-wrap square">
                 <img src="https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-cards-snapshots-1558099940_tlUmVe.jpeg/320/320">
               </div>
             </div>
           
-          <p class="questions-item__content text-caption-1">티비다이를 너무짧은걸샀나봐요..왼쪽을 어떻게 꾸밀지고민이에오ㅡ..화분을놓고 싶기도하고물티슈등 수납공간이부족하여수납가능한 선반을 놓고싶기도한데..맞는컬러가 무엇일까요..<br>네이비 인테리어하기 힘ㄷ느네요봄되니..더워보여요..</p>
+          <p id="first">티비다이를 너무짧은걸샀나봐요..왼쪽을 어떻게 꾸밀지고민이에오ㅡ..화분을놓고 싶기도하고 물티슈등 수납공간이부족하여수납가능한 선반을 놓고싶기도한데..맞는컬러가 무엇일까요..<br>네이비 인테리어하기 힘ㄷ느네요봄되니..더워보여요..</p>
         </article>  
         <hr>
     <article class="questions-item">
@@ -24,7 +25,7 @@
           <p class="questions-item__content text-caption-1">제가 침대와 철제선반 사이에 이케아 스탠드를 놓았는데 코드 꼿는데가 옷장있는쪽이라 선이 문제가되서 이참에 가구배치를 다시 하고싶어요.<br>어떻게 하면 좋을까요?인테리어 능력자님들 도와주세요~제발 댓글부탁드려요~~♡-♡</p>
 
         </article>
-        <hr>
+                <hr>
         
         <article class="questions-item">
           <h1 class="questions-item__title text-heading-5 bold text-black">작은 시골집 리모델링 </h1>
@@ -64,7 +65,7 @@
 <style lang="scss">
   
 button {
-  background-color: #4CAF50; /* Green */
+  background-color: rgb(80, 123, 163); /* Green */
   border: none;
   color: white;
   padding: 15px 32px;
@@ -74,7 +75,18 @@ button {
   font-size: 16px;
 }
 h1:hover{
-  background-color: black;
-  color: white;
+  background-color: white;
+  color: black;
+}
+h1:visited{
+  text-decoration: none;
+    
+}
+div{
+  margin: 30px;
+}
+#first{
+  font-weight: bold;
+  text-align: center;
 }
 </style>
---
+++ src/views/Communitydetail.vue
@@ -0,0 +1,46 @@
+<template>
+
+    <section id="question__content" class="page-2col__content">
+    <header class="question__content__header page-2col__content__header">
+    
+      <h5>질문과 답변</h5>
+      <h3>Question: 거실 전면인테리어 ㅜㅜ도와주세요</h3>
+      <p id="iid">작성자: 어린쥐</p>
+    </header>
+    <section class="question__content__body">
+            <img src="https://image.ohou.se/image/resize/bucketplace-v2-development/uploads-cards-snapshots-1558099940_tlUmVe.jpeg/1700/none" alt="None" width="45%" height="400px">
+            <img src="https://image.ohou.se/image/resize/bucketplace-v2-development/uploads-cards-snapshots-1558099945_If6.jpeg/1700/none" alt="None" width="45%" height="400px">
+    </section>
+    <h3>Answer</h3>
+
+    <div>
+
+    </div>
+  </section>
+</template>
+
+
+<style lang="scss">
+pre{
+    font-size: 19px;
+}
+h5{
+    font-size: 24px;
+    color: rgb(80, 123, 163);
+    text-decoration: underline;
+}
+h3{
+    margin-left: 5%;
+    margin-right: 5%;
+    font-size:21px;
+    color: white;
+    background-color: rgb(80, 123, 163);
+    padding: 20px;
+}
+#iid{
+    font-size: 18px;
+    text-align: right;
+    font-weight: bold;
+    margin-right: 6%;
+}
+</style>
