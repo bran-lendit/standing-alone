f0e26c33fcd3259390f42c8f03691923dc335ea7
Hyun Am <tlfgjawlq@naver.com>
2019-05-17 23:18:49 +0900

    store-vue 수정

--- src/views/Store.vue
+++ src/views/Store.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="store">
-    <h1>This is an about page</h1>
+    <div>
+	    <img width="300" height="300" src="https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1540896748908_W.jpg/360/360" srcset="https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1540896748908_W.jpg/640/640 1.5x,https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1540896748908_W.jpg/720/720 2x,https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1540896748908_W.jpg/1080/1080 3x">
+	    <img width="300" height="300" src="https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1552474510756_l3FF.jpg/360/360" srcset="https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1552474510756_l3FF.jpg/640/640 1.5x,https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1552474510756_l3FF.jpg/720/720 2x,https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1552474510756_l3FF.jpg/1080/1080 3x">
+	    <img width="300" height="300" src="https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1557729174495_pxx0b1z.jpg/360/360" srcset="https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1557729174495_pxx0b1z.jpg/640/640 1.5x,https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1557729174495_pxx0b1z.jpg/720/720 2x,https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1557729174495_pxx0b1z.jpg/1080/1080 3x">
+	    <img width="300" height="300" src="https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1557729174495_pxx0b1z.jpg/360/360" srcset="https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1557729174495_pxx0b1z.jpg/640/640 1.5x,https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1557729174495_pxx0b1z.jpg/720/720 2x,https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1557729174495_pxx0b1z.jpg/1080/1080 3x">
+	    <img width="300" height="300" src="https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1553567374465_KpbCbNXl.jpg/360/360" srcset="https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1553567374465_KpbCbNXl.jpg/640/640 1.5x,https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1553567374465_KpbCbNXl.jpg/720/720 2x,https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-productions-1553567374465_KpbCbNXl.jpg/1080/1080 3x">
   </div>
+  </div>
+  
 </template>
+<style lang="scss" scoped>
+.store {
+  max-width: 900px;
+  margin: 0 auto;
+  div {
+	  display: flex;
+	  justify-content: center; /* flex-start, center,  	flex-end, space-between, space-around */
+	  justify-content: space-around;
+    flex-wrap: wrap;
+    min-width: 900px;
+  }
+}
+
+</style>
